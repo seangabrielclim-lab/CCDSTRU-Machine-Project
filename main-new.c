c2c0b49f20ed3642a1e0953372301a8342920582
elianamorin <eliana_katarina_morin@dlsu.edu.ph>
2026-03-29 14:38:55 +0800

    Add files via upload

--- main-new.c
+++ main-new.c
@@ -8,8 +8,7 @@ void printBoard(char board[SIZE][SIZE])
     int i, j;
 
     printf("\n==========================\n");
-    printf("||                      ||\n");
-    printf("||        0 1 2         ||\n");
+    printf("||        1 2 3         ||\n");
     for (i = 0; i < SIZE; i++)
     {
         printf("||      %d ", i);
@@ -19,254 +18,325 @@ void printBoard(char board[SIZE][SIZE])
         }
         printf("        ||\n");
     }
-    printf("||                      ||\n");
     printf("==========================\n");
 }
 
 /* ===================== UTILITY ===================== */
-
 int inBounds(int r, int c)
 {
-    return (r >= 0 && r < 3 && c >= 0 && c < 3);
-}
+    int result;
 
-int checkIfFree(char board[3][3], int row, int column)
-{
-    int result = 0;
-
-    if (inBounds(row, column))
-    {
-        if (board[row][column] == '.')
-            result = 1;
-    }
+    if (r >= 0 && r < SIZE && c >= 0 && c < SIZE)
+        result = 1;
+    else
+        result = 0;
 
     return result;
 }
 
-int countFree(char board[3][3])
+int checkIfFree(char board[SIZE][SIZE], int r, int c)
+{
+    int result;
+
+    if (inBounds(r, c) && board[r][c] == '.')
+        result = 1;
+    else
+        result = 0;
+
+    return result;
+}
+
+int countFree(char board[SIZE][SIZE])
 {
     int i, j, count = 0;
 
-    for (i = 0; i < 3; i++)
-        for (j = 0; j < 3; j++)
+    for (i = 0; i < SIZE; i++)
+    {
+        for (j = 0; j < SIZE; j++)
+        {
             if (board[i][j] == '.')
                 count++;
+        }
+    }
 
     return count;
 }
 
-int countOwned(char board[3][3], char player)
+int countOwned(char board[SIZE][SIZE], char player)
 {
     int i, j, count = 0;
 
-    for (i = 0; i < 3; i++)
-        for (j = 0; j < 3; j++)
+    for (i = 0; i < SIZE; i++)
+    {
+        for (j = 0; j < SIZE; j++)
+        {
             if (board[i][j] == player)
                 count++;
+        }
+    }
 
     return count;
 }
 
 /* ===================== REMOVE ===================== */
-
-void removePos(char board[3][3], int r, int c)
+void removePos(char board[SIZE][SIZE],
+               int S[SIZE][SIZE],
+               int T[SIZE][SIZE],
+               int r, int c)
 {
     if (inBounds(r, c))
+    {
         board[r][c] = '.';
+        S[r][c] = 0;
+        T[r][c] = 0;
+    }
 }
 
-/* ===================== REPLACE ===================== */
+/* ===================== FORWARD DECL ===================== */
+void expand(char board[SIZE][SIZE],
+            int S[SIZE][SIZE],
+            int T[SIZE][SIZE],
+            int r, int c, char player, int *found);
 
-void replacePos(char board[3][3], int r, int c, char player)
+/* ===================== REPLACE ===================== */
+void replacePos(char board[SIZE][SIZE],
+                int S[SIZE][SIZE],
+                int T[SIZE][SIZE],
+                int r, int c, char player, int *found)
 {
-	char opponent;
-	
-    if(player == 'R')
-    {
+    char opponent;
+    int valid;
+
+    if (player == 'R')
         opponent = 'B';
-    }
     else
-    {
         opponent = 'R';
-    }
-    
-    if(inBounds(r,c))
+
+    *found = 0;
+    valid = inBounds(r, c);
+
+    if (valid)
     {
-	    if (board[r][c] == opponent)
-	        board[r][c] = player;
-	
-	    else if (board[r][c] == '.')
-	        board[r][c] = player;
-	}
+        if (board[r][c] == opponent)
+        {
+            board[r][c] = player;
+            *found = 1;
+        }
+        else if (board[r][c] == player)
+        {
+            *found = 1;
+        }
+        else
+        {
+            board[r][c] = player;
+        }
+
+        if (*found && S[r][c] == 0)
+        {
+            S[r][c] = 1;
+            *found = 0;
+        }
+        else if (*found && S[r][c] == 1 && T[r][c] == 0)
+        {
+            T[r][c] = 1;
+            expand(board, S, T, r, c, player, found);
+        }
+    }
 }
 
 /* ===================== EXPAND ===================== */
-
-void expand(char board[3][3], int r, int c, char player)
+void expand(char board[SIZE][SIZE],
+            int S[SIZE][SIZE],
+            int T[SIZE][SIZE],
+            int r, int c, char player, int *found)
 {
-    int up = r - 1;
-    int down = r + 1;
-    int left = c - 1;
-    int right = c + 1;
+    int up, down, left, right;
 
-    removePos(board, r, c);
+    up = r - 1;
+    down = r + 1;
+    left = c - 1;
+    right = c + 1;
+
+    removePos(board, S, T, r, c);
 
     if (player == 'R')
-        replacePos(board, up, c, player);
-
+        replacePos(board, S, T, up, c, player, found);
     else
-        replacePos(board, down, c, player);
+        replacePos(board, S, T, down, c, player, found);
 
-    replacePos(board, r, left, player);
-    replacePos(board, r, right, player);
+    replacePos(board, S, T, r, left, player, found);
+    replacePos(board, S, T, r, right, player, found);
 }
 
 /* ===================== UPDATE ===================== */
-
-void update(char board[3][3], int r, int c, char player, int expanded[3][3])
+void update(char board[SIZE][SIZE],
+            int S[SIZE][SIZE],
+            int T[SIZE][SIZE],
+            int r, int c, char player, int *good)
 {
-    if (!expanded[r][c])
+    int found;
+    int condition;
+
+    *good = 0;
+
+    if (S[r][c] == 0)
     {
-        expanded[r][c] = 1;
-        expand(board, r, c, player);
+        S[r][c] = 1;
+        *good = 1;
+    }
+
+    condition = (!(*good) && S[r][c] == 1 && T[r][c] == 0);
+
+    if (condition)
+    {
+        T[r][c] = 1;
+        found = 0;
+        expand(board, S, T, r, c, player, &found);
     }
 }
 
-/* ===================== MOVE INPUT ===================== */
-
-void getMove(int *row, int *col)
+/* ===================== INPUT ===================== */
+void getMove(int *r, int *c)
 {
     printf("Enter row: ");
-    scanf("%d", row);
+    scanf("%d", r);
+    (*r)--;
 
     printf("Enter column: ");
-    scanf("%d", col);
+    scanf("%d", c);
+    (*c)--;
 }
 
 /* ===================== PLAYER TURN ===================== */
-
-void playerTurn(
-    char board[3][3],
-    int *moveCounter,
-    int *startPhase,
-    int *go,
-    int expanded[3][3])
+void playerTurn(char board[SIZE][SIZE],
+                int S[SIZE][SIZE],
+                int T[SIZE][SIZE],
+                int *val,
+                int *start,
+                int *go)
 {
-    int row, col;
-    char player = (*go) ? 'R' : 'B';
-    int validMove = 0;
+    int r, c;
+    int done;
+    int good;
+    char player;
+
+    done = 0;
+    player = (*go) ? 'R' : 'B';
 
     printf("\n~~~ %c'S TURN ~~~\n", player);
 
-    while (!validMove)
+    while (!done)
     {
-        getMove(&row, &col);
+        getMove(&r, &c);
 
-        if (!inBounds(row, col))
+        if (!inBounds(r, c))
         {
             printf("\n!! Invalid coordinates. Try again.\n");
         }
         else
         {
-            if (*startPhase) 													/*START PHASE*/
-            {
-                if (checkIfFree(board, row, col))
-                {
-                    board[row][col] = player;
-                    (*moveCounter)++;
+            good = 0;
 
-                    printf("\nValid move.\n");
-                    printBoard(board);
+            if (*start)
+            {
+                if (checkIfFree(board, r, c))
+                {
+                    board[r][c] = player;
+                    S[r][c] = 1;
+                    good = 1;
 
                     if (countOwned(board, 'R') == 1 &&
                         countOwned(board, 'B') == 1)
                     {
-                        *startPhase = 0;
+                        *start = 0;
                         printf("\nStart phase complete.\n");
-                    }
 
-                    *go = !(*go);
-                    validMove = 1;
+                    }
                 }
                 else
                 {
                     printf("\n!! Position not free. Try again.\n");
                 }
             }
-            else																/*NORMAL PHASE*/
+            else
             {
-                if ((*go && board[row][col] != 'R') ||
-                    (!(*go) && board[row][col] != 'B'))
+                if ((*go && board[r][c] == 'R') ||
+                    (!*go && board[r][c] == 'B'))
                 {
-                    printf("\n!! You can only select your own position.\n");
+                    update(board, S, T, r, c, player, &good);
                 }
                 else
                 {
-                    update(board, row, col, player, expanded);
-                    (*moveCounter)++;
-
-                    printBoard(board);
-
-                    *go = !(*go);
-                    validMove = 1;
+                    printf("\n!! You can only select your own position.\n");
                 }
+            }
+
+            if (good)
+            {
+                *go = !(*go);
+                (*val)++;
+                done = 1;
             }
         }
     }
 }
 
 /* ===================== GAME OVER ===================== */
-
-int gameOver(char board[3][3], int moveCounter, int startPhase)
+int gameOver(char board[SIZE][SIZE], int val, int start)
 {
-    int free = countFree(board);
-    int r = countOwned(board, 'R');
-    int b = countOwned(board, 'B');
-    int over = 0;
+    int free, r, b;
+    int over;
+
+    free = countFree(board);
+    r = countOwned(board, 'R');
+    b = countOwned(board, 'B');
+
+    over = 0;
 
     if (free == 3)
         over = 1;
 
-    if (moveCounter >= 20)
+    if (val >= 20)
         over = 1;
 
-    if (!startPhase && ((r > 0 && b == 0) || (b > 0 && r == 0)))
+    if (!start && ((r > 0 && b == 0) || (b > 0 && r == 0)))
         over = 1;
 
     return over;
 }
 
 /* ===================== RESULT ===================== */
-
-void printResult(char board[3][3])
+void printResult(char board[SIZE][SIZE])
 {
-    int r = countOwned(board, 'R');
-    int b = countOwned(board, 'B');
+    int r, b;
+
+    r = countOwned(board, 'R');
+    b = countOwned(board, 'B');
 
     if (r > b)
         printf("\nR wins\n");
-
     else if (b > r)
         printf("\nB wins\n");
-
     else
         printf("\nDraw\n");
 }
 
 /* ===================== MAIN ===================== */
-
 int main()
 {
-    char board[3][3] = {
+    char board[SIZE][SIZE] = {
         {'.', '.', '.'},
         {'.', '.', '.'},
         {'.', '.', '.'}};
 
-    int expanded[3][3] = {0};
+    int S[SIZE][SIZE] = {0};
+    int T[SIZE][SIZE] = {0};
 
-    int moveCounter = 0;
-    int startPhase = 1;
-    int go = 1; /* 1 = Red, 0 = Blue */
+    int val, start, go;
+
+    val = 0;
+    start = 1;
+    go = 1;  /* 1 = Red, 0 = Blue */
 
     printf("=============================================================\n");
     printf("                         START GAME                          \n");
@@ -278,19 +348,16 @@ int main()
     printf(". = Free\n");
 
     printf("\nRed goes first.\n");
-
+    
     printBoard(board);
 
-    while (!gameOver(board, moveCounter, startPhase))
+    while (!gameOver(board, val, start))
     {
-        printf("\nMove Counter: %d\n", moveCounter);
+        printf("\nMove Counter: %d\n", val);
+        printf("Phase: %s\n", start ? "START" : "NORMAL");
 
-        if (startPhase)
-            printf("Game Phase: START\n");
-        else
-            printf("Game Phase: NORMAL\n");
-
-        playerTurn(board, &moveCounter, &startPhase, &go, expanded);
+        playerTurn(board, S, T, &val, &start, &go);
+        printBoard(board);
     }
 
     printf("\n=============================================================\n");
